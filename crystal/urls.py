c3779ea91b9265785b4f486e8c1842dc09dbdef5
Mark Roman Miller <725mrm@gmail.com>
2020-04-17 18:03:17 -0700

    still chunking commits bc of that dashboard lol

--- crystal/urls.py
+++ crystal/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('rank/', views.rank, name='rank'),
+    path('', views.inbox, name='inbox'),
     path('make_cites/<int:publication_id>/', views.make_cites, name='make_cites'),
     path('make_cited_by/<int:publication_id>/', views.make_cited_by, name='make_cited_by'),
-    path('show_pdf/<int:publication_id>/', views.show_pdf, name="show_pdf")
+    path('show_pdf/<int:publication_id>/', views.show_pdf, name="show_pdf"),
+    path('pub_status/<int:publication_pk>/<status>/', views.pub_status, name="pub_status"),
 ]
--- crystal/views.py
+++ crystal/views.py
@@ -1,25 +1,23 @@
 from django.http import HttpResponse, FileResponse, Http404
 from django.shortcuts import render, redirect
 from .models import Publication, Profile
+from django.core.exceptions import ValidationError
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the Golgi index.")
-
-
-def rank(request):
+def inbox(request):
     user = request.user
-    if user is None:
-        return HttpResponse("Please log in")
+    if user.is_anonymous:
+        return HttpResponse("Please <a href='admin/login/?next=/'>log in</a>")
 
     network = Profile.objects.get(user=user).active_network
     if network is None:
         return HttpResponse("Choose an active network.")
 
-    related_publications = [p for p in network.publication_set.all() if p.n_related()]
+    related_publications = [p for p in network.publication_set.filter(network_status=Publication.SUGGESTED)]
     related_publications.sort(key=lambda p : p.n_related(), reverse=True)
 
     context = {
+        'network': network,
         'selected_publications': network.publication_set.filter(network_status=Publication.INCLUDED),
         'uploaded_publications': network.publication_set.filter(network_status=Publication.UPLOADED),
         'related_publications': related_publications
@@ -31,14 +29,14 @@ def make_cites(request, publication_id):
     pub = Publication.objects.get(pk=publication_id)
     print(pub)
     pub.make_cites()
-    return redirect('rank')
+    return redirect('inbox')
 
 
 def make_cited_by(request, publication_id):
     pub = Publication.objects.get(pk=publication_id)
     print(pub)
     pub.make_cited_by()
-    return redirect('rank')
+    return redirect('inbox')
 
 
 def show_pdf(request, publication_id):
@@ -47,4 +45,15 @@ def show_pdf(request, publication_id):
         return FileResponse(open(pub.file.path, 'rb'), content_type='application/pdf')
     except ValueError:
         raise Http404("File has not been uploaded.")
+
+
+def pub_status(request, publication_pk, status):
+    pub = Publication.objects.get(pk=publication_pk)
+    pub.network_status = status
+    try:
+        pub.clean_fields()
+    except ValidationError:
+        return HttpResponse('Cleaning failed, could not change pub status. <a href="/">Return to home.</a>')
+    pub.save()
+    return redirect('inbox')
 
